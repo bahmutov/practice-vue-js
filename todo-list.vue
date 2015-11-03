133e146a0ebb7e3bdf1250cffca2d85f4a71556b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-03 11:00:03 -0500

    started component

---
+++ todo-list.vue
@@ -0,0 +1,39 @@
+<style>
+.todo-list ul {
+  list-style-type: 'none';
+}
+</style>
+
+<template>
+  <input v-model="newTodo" @keyup.enter="addTodo">
+  <ul>
+    <li v-for="todo in todos">
+      <span>{{ todo.text }}</span>
+      <button @click="removeTodo($index)">X</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+module.exports = {
+  el: '#todo-app',
+  data: {
+    newTodo: '',
+    todos: [
+      { text: 'Add some todos' }
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+};
+</script>
